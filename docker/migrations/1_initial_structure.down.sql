40cf3f28579ed9e48e3118ca6ed6bd1386f18d87
Piero Valdebenito <piero.valdebenito@adevinta.com>
2020-02-28 14:57:18 -0300

    [feat/set-config] - added index for unique active product per email

--- docker/migrations/1_initial_structure.down.sql
+++ docker/migrations/1_initial_structure.down.sql
@@ -1,6 +1,7 @@
 DROP INDEX IF EXISTS user_product_user_id_idx;
 DROP INDEX IF EXISTS user_product_user_email_idx;
 DROP INDEX user_product_unique_active_product;
+DROP INDEX user_product_unique_active_product_per_email;
 DROP TABLE IF EXISTS user_product_config;
 DROP TABLE IF EXISTS user_product;
 DROP TYPE enum_product_type;
--- docker/migrations/1_initial_structure.up.sql
+++ docker/migrations/1_initial_structure.up.sql
@@ -28,8 +28,11 @@ CREATE TABLE IF NOT EXISTS user_product(
 	comment         TEXT
 );
 
--- create index to allow only one active product type per user
+-- create index to allow only one active product type per user_id
 CREATE unique index user_product_unique_active_product on user_product(product_type, user_id, status)
+    where status = 'ACTIVE';
+-- create index to allow only one active product type per user_email
+CREATE unique index user_product_unique_active_product_per_email on user_product(product_type, user_email, status)
     where status = 'ACTIVE';
 
 CREATE TABLE IF NOT EXISTS user_product_config(
