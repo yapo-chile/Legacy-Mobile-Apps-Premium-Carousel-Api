c2fe3e133d7a320bb3044ab6b692b4679ddcfa01
luis teran <luis.teran@adevinta.com>
2020-03-17 15:10:15 -0300

    [feat/change-product-update-json] Added debug message

--- pkg/interfaces/handlers/setConfig.go
+++ pkg/interfaces/handlers/setConfig.go
@@ -42,6 +42,7 @@ type setConfigRequestOutput struct {
 func (*SetConfigHandler) Input(ir InputRequest) HandlerInput {
 	input := setConfigHandlerInput{}
 	ir.Set(&input).FromJSONBody().FromPath()
+	fmt.Printf(`set config input: %+v`, input)
 	return &input
 }
 
